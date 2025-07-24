6472eea6224bb3bbc18681698f973a1781e5d8a7
jamesjo <jamescakr@gmail.com>
2025-07-24 11:52:56 -0600

    first commit for todo BE

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
+const app = express();
+app.use(bodyParser.json());
+app.use("/api", indexRouter);
+const mongoURI = `mongodb://localhost:27017/todo-250724`;
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("mongoose connected!");
+  })
+  .catch((err) => {
+    console.log("DB connection fail!");
+  });
+
+app.listen(4000, () => {
+  console.log("server is on 4000");
+});
